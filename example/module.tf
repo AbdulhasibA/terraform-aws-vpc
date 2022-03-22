15c36328722053878af0b9bacd7421dfb1acf83c
AbdulhasibA <96264417+AbdulhasibA@users.noreply.github.com>
2022-03-21 19:26:44 -0500

    added subnets

--- example/module.tf
+++ example/module.tf
@@ -1,6 +1,7 @@
 module "vpc" {
     source = "../"
     cidr_block = var.cidr_block
+    private_subnets_cidr = ["10.0.1.0/24"]
     tags = {
         Name = "Dev"
     }
--- variable.tf
+++ variable.tf
@@ -2,3 +2,7 @@ variable "cidr_block" {}
 variable "tags" {
     type = map
 }
+
+variiable "private_subnets_cidr" {
+    type = list
+}
